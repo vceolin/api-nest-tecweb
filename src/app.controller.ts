cdce94b9c1a99902bcafd8c3805aaaaad9553c53
Vitor Ceolin <vceolinn@gmail.com>
2021-11-20 18:17:46 -0300

    refactor: use correct variables and return better errors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get(':alimento')
-  @ApiResponse({ status: 200, description: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Bananas_%28Alabama_Extension%29.jpg'})
+  @ApiResponse({ status: 200, description: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Bananas_%28Alabama_Extension%29.jpg' })
+  @ApiResponse({ status: 404, description: 'Image not found' })
   @ApiOperation({ summary: 'retorna o link de uma imagem do alimento solicitado por parâmetro' })
   async getFotoAlimento(@Param('alimento') alimento: string): Promise<string> {
     return await this.appService.getFotoAlimento(alimento);
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import fetch from 'node-fetch';
 @Injectable()
 export class AppService {
   async getFotoAlimento(alimento: string): Promise<string> {
-    const response = await fetch('https://api.unsplash.com/search/photos?page=1&query=banana&client_id=-HZSVNde3TaH4iXUtrcOZKLTvwyKb7O8gTLtQOEbUIA&per_page=1&orientation=squarish');
+    const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${alimento}&client_id=-HZSVNde3TaH4iXUtrcOZKLTvwyKb7O8gTLtQOEbUIA&per_page=1&orientation=squarish&lang=pt`);
     const data = await response.json();
+    if (data.total === 0) throw new NotFoundException('Image not found')
     return data.results[0].urls.regular;
   }
 }
