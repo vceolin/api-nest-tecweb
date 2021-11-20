59592164d941aed24f884594aed9a6e2deb9ea76
RodyCast <rodrigocastilhomagalhaes@gmail.com>
2021-11-20 16:27:56 -0300

    feat: add getFotoAlimento endpoint
    Co-Authored-By: Vitor Ceolin <39313422+vceolin@users.noreply.github.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import fetch from 'node-fetch';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getFotoAlimento(alimento: string): Promise<string> {
+    const response = await fetch('https://api.unsplash.com/search/photos?page=1&query=banana&client_id=-HZSVNde3TaH4iXUtrcOZKLTvwyKb7O8gTLtQOEbUIA&per_page=1&orientation=squarish');
+    const data = await response.json();
+    return data.results[0].urls.regular;
   }
 }
